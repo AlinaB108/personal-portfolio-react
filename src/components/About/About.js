9b0eab71c8b90294488e5f870581bd516e4ec100
Alina Badillo <alinachristabel108@gmail.com>
2023-09-12 22:55:09 -0500

    Added about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import '../../App.css';
 import './About.css';
+import { AppBar, Box, Toolbar, Typography, Button, Grid } from '@mui/material';
+import wintersummer from "../../assets/images/wintersummer.jpg";
 
 export default function About() {
   return (
-    <div>
-      <h2>I'm about me section</h2>
-    </div>
+    <Grid container justifyContent="center" textAlign="center" sx={{ my:5, p:5 }}>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h5">
+          About
+        </Typography>
+        <Typography variant="h5">
+          Alina Badillo - Web Developer
+        </Typography>
+        <Typography variant="h6">
+          A Siberian who lives in Texas.
+        </Typography>
+        <Typography variant="h6">
+        My love for computers and technology has existed since I was first introduced to them by my uncle as a young child. 
+        Growing up, I have enjoyed spending countless hours in front of the screen, learning, playing and expanding my knowledge in any and all interests. 
+        Even though I received an education in languages, I decided to pursue my passion and become a Web Developer.
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Grid>
+        <img src={wintersummer} width='60%' height='auto' style={{ borderRadius: '25px'}} className='me'/>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
