059832d4ab3e07ae2b58f7c9794a87e1f4cd1895
Alina Badillo <alinachristabel108@gmail.com>
2023-09-12 21:16:01 -0500

    Added Material UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header.js';
 import About from './components/About/About.js';
 import Projects from './components/Projects/Projects.js';
+import Contact from './components/Contact/Contact.js';
 import Footer from './components/Footer/Footer.js';
 
 // All we want to render to the screen
@@ -13,6 +14,7 @@ export default function App() {
       <Header />
       <About />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import './Contact.css';
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>My contact form will be here</h2>
+    </div>
+  );
+}
